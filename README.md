162d81f82884d1c7cef8307254a7d0b38226a3ac
Christopher Liu <christopherliu830@gmail.com>
2020-05-26 13:58:32 -0400

    refactor to make more user friendly

--- README.md
+++ README.md
@@ -3,8 +3,18 @@ Python
 Selenium
 Chrome webdriver
 
-To create csv files (separated by pages): run csv-scraper.py
+## usage
+run wiley.py [location of keys_a.txt] [location of keys_b.txt]
 
-To batch csv files: run csv-collector.py
+## options
+-l, --log : print the current log data 
+-r, --results : scrape result counts only
+-p, --procs (default 8) : how many simultaneous selenium processes
+-d, --database (default False) : take current scraped data and export. If false, will finish scraping before export.
 
-To create database: run create-database.py
+
+## version 1.1
+new features -
+    crash recovery, integrity checks
+    automatic structuring of files
+    more robust logging system
---
+++ wiley.py
@@ -0,0 +1,36 @@
+import argparse
+import sys
+import os
+from lib import integrity, page_scraper, database, plog 
+
+def read_keys(keys_path):
+    with open(keys_path, 'r', encoding='utf-8') as keyfile:
+        return [line.strip() for line in keyfile]
+
+def main():
+    parser = argparse.ArgumentParser(description='Wiley Online Library scraping interface.')
+    parser.add_argument('a', metavar='keys-a', type=str)
+    parser.add_argument('b', metavar='keys-b', type=str)
+    parser.add_argument('-l', '--log', help='print current log', action='store_true')
+    parser.add_argument('-r', '--results', help='get result count only', action='store_true')
+    parser.add_argument('-p', '--procs', type=int, help='number of simultaneous selenium processes', default=8)
+    parser.add_argument('-d', '--database', help='skip scraping and export to database', action='store_true')
+
+    args = parser.parse_args()
+    keysa = read_keys(args.a)
+    keysb = read_keys(args.b)
+    
+    integrity.integrity_check(os.getcwd(), keysa, keysb)
+
+    with plog.ProgressLog('data/progress.log') as log:
+        if args.log:
+            print(log)
+            return
+        paginated_path = os.path.join(os.getcwd(), 'data', 'paginated')
+        database_path = os.path.join(os.getcwd(), 'data', 'wiley.db')
+        if not args.database:
+            page_scraper.start(paginated_path, keysa, keysb, log, results_only=args.results, procs=args.procs)
+        database.export(paginated_path, database_path, keysa, keysb, log)
+
+if __name__ == '__main__':
+    main()
